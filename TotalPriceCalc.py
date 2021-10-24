33388a78027a8a489c6878af19e2eb031858fd0a
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-24 18:21:22 +0800

    Edited -- Ver1.2

--- TotalPriceCalc.py
+++ TotalPriceCalc.py
@@ -1,8 +1,11 @@
 QuantApl = int(input("How many apples? \n> "))
 QuantOrng = int(input("\nHow many oranges? \n> "))
 
-ttlprc_apl = QuantApl*20
-ttlprc_orng = QuantOrng*25
+apl_price = 20
+orng_price = 25
+
+ttlprc_apl = QuantApl*apl_price
+ttlprc_orng = QuantOrng*orng_price
 grnd_total = ttlprc_apl + ttlprc_orng
 
 print(f"\nThe total amount is {grnd_total}.")
