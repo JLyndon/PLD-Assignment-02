2b23284c832100d3d7cac1ffed126b284ab53289
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-24 18:10:57 +0800

    Edited -- Code Completed

--- UserDetails.py
+++ UserDetails.py
@@ -1,5 +1,5 @@
 usr_name = input("Enter your name: ")
-usr_age = int(input("Enter age: "))
+usr_age = input("Enter age: ")
 usr_address = input("Enter your address: ")
 
 print(f"\nHi, my name is {usr_name}. I am {usr_age} years old and I live in {usr_address}.")
