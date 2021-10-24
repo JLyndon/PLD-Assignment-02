859b5edf6cb6f09057f74c9d16239d6ceeec9870
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-24 18:08:46 +0800

    Edit -- Code Completed

--- MaxAppleQuan.py
+++ MaxAppleQuan.py
@@ -1,9 +1,7 @@
-import math
-
 usr_balance = float(input("Enter your balance: "))
 apl_price = float(input("Enter the price of an apple: "))
 
-max_no_apl = math.floor(usr_balance/apl_price)
-change = float(usr_balance%apl_price)
+max_no_apl = int(usr_balance//apl_price) #Replaced math.floor with double slash
+change = usr_balance % apl_price
 
-print(f"\nYou can buy {max_no_apl} apples and your change is {change: .2f} pesos.")
+print(f"\nYou can buy {max_no_apl} apples and your change is {change:.2f} pesos.")
